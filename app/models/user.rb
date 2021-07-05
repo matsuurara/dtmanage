fa43d143861b7f9d6456efd3c2fed21628763067
matsuurara <tact1997asdf@gmail.com>
2021-07-05 23:10:46 +0900

    バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+        with_options presence: true do
+          validates :nickname
+          validates :password,
+                    format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+          with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+            validates :last_name
+            validates :first_name
+          end
+          validates :birthday
+        end
+
 end
